5a45c0b30ccb1a618e0d18f42088e1067f646a51
Evgeniy28 <azarov91@mail.ru>
2015-03-28 10:37:10 +0300

    Add green test Home page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,10 +1,20 @@
-require 'spec_helper'
+require 'rails_helper'
 
-describe "StaticPages" do
-  describe "Home pages" do
-    it "should have the content 'iWebLab App' " do
-      visit '/static_pages/home'
-      expect(page).to have_content('iWebLab Appcdcrails')
-    end
+
+# describe "Static pages" do
+
+# 	describe "Home page" do
+
+# 		it "should have the content 'iWebLab'" do
+# 			visit '/static_pages/home'
+# 			expect(page).to have_content('iWebLab')
+# 		end
+# 	end
+# end
+
+RSpec.describe "home page", :type => :request do
+  it "should have the content 'iWebLab'" do
+    visit "/static_pages/home"
+    expect(page).to have_content("iWebLab")
   end
 end
