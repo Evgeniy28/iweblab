aed1876b6b6b91da63ddaefff59af3a53b14ff58
Evgeniy28 <azarov91@mail.ru>
2015-03-31 00:40:23 +0300

    Add signup page with rout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
 
+	get 'users/new'
+
 	root 'static_pages#home'
 	get 'courses', to: 'static_pages#courses', as: :courses
+	get 'signup',  to: 'users#new',            as: :signup
 
 	# The priority is based upon order of creation: first created -> highest priority.
 	# See how all your routes lay out with "rake routes".
