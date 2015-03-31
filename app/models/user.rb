0dac7b381c243beb797dede5a4ecc7e2debb1249
Evgeniy28 <azarov91@mail.ru>
2015-03-31 22:41:21 +0300

    Add validates name, email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+	validates :name, presence: true, length: { maximum: 50 }
+
+	#здесь рег-ое вырю это const, обеспеч. допуст-ть адресов соотв. образцу
+	#остальные будут считаться недопустимыми
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
 end
