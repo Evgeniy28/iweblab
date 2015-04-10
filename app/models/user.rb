34941816e2fd6faa674cfc2c2d2c7003b33afacf
Evgeniy28 <azarov91@mail.ru>
2015-04-11 00:32:12 +0300

    Add green test uniqueness email

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ActiveRecord::Base
 	#здесь рег-ое вырю это const, обеспеч. допуст-ть адресов соотв. образцу
 	#остальные будут считаться недопустимыми
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+										uniqueness: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -47,4 +47,13 @@ RSpec.describe User, type: :model do
 		end
 	end
 
+	describe "when email address is already taken" do
+		before do
+			user_with_same_email = @user.dup #создаем дубликат пользователя с теми же атрибутами
+			user_with_same_email.save #сохраняем пользователя с адресом, который уже есть в базе данных
+		end
+
+		it { should_not be_valid }
+	end
+
 end
