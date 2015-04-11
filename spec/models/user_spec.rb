067b5da26deb7599cc431e5c018276c4bb8a8bf1
Evgeniy28 <azarov91@mail.ru>
2015-04-11 03:13:41 +0300

    Add has_secure_password

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-	before { @user = User.new(name: "Evgeniy Azarov",
-														email: "azarov91@mail.ru") }
+	before do @user = User.new(name: "Evgeniy Azarov",
+														email: "azarov91@mail.ru",
+														password: "1235qewa",
+														password_confirmation: "1235qewa")
+	end
 
 	subject { @user }
 
 	it { should respond_to(:name) } #respond_to принимает символ и возвращает true, если объект отвечает на данный метод, иначе false(проверяет в объекте наличие такой переменной)
 	it { should respond_to(:email) }
 	it { should respond_to(:password_digest) }
+	it { should respond_to(:password) }
+	it { should respond_to(:password_confirmation) }
 
 	it { should be_valid } #проверка что объект @user изначально валиден
 
@@ -64,6 +69,20 @@ RSpec.describe User, type: :model do
 			user_with_same_email.save #сохраняем пользователя с адресом, который уже есть в базе данных
 		end
 
+		it { should_not be_valid }
+	end
+
+	describe "when password is not present" do
+		before do @user = User.new(name: "Evgeniy Azarov",
+															email: "azarov91@mail.ru",
+															password: "",
+															password_confirmation: "")
+		end
+		it { should_not be_valid }
+	end
+
+	describe "when password doesn't match confirmation" do
+		before { @user.password_confirmation = "mismatch" }
 		it { should_not be_valid }
 	end
 
